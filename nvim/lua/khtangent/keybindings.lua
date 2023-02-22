8d0b807b1c237c4296170b4d8f511e7d2f56501b
KHTangent <contact@khtangent.com>
2023-02-22 09:59:25 +0100

    Small Neovim config update

--- nvim/lua/khtangent/keybindings.lua
+++ nvim/lua/khtangent/keybindings.lua
@@ -8,6 +8,9 @@ vim.keymap.set("v", "æ", "$")
 vim.keymap.set("o", "ø", "^")
 vim.keymap.set("o", "æ", "$")
 
+vim.keymap.set("n", "<M-j>", "<C-E>")
+vim.keymap.set("n", "<M-k>", "<C-Y>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- nvim/lua/khtangent/preferences.lua
+++ nvim/lua/khtangent/preferences.lua
@@ -1,5 +1,6 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
