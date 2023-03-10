468fb34bc206400985b378710dce0cc8ff3e20aa
KHTangent <contact@khtangent.com>
2023-03-10 09:32:28 +0100

    Add more nvim plugins and config

---
+++ nvim/after/plugin/neotree.lua
@@ -0,0 +1,22 @@
+require("neo-tree").setup({
+	close_if_last_window = true,
+	enable_git_status = true,
+	window = {
+		width = 30
+	},
+	filesystem = {
+		filtered_items = {
+			hide_dotfiles = false,
+			hide_gitignored = false,
+			hide_hidden = false
+		},
+		follow_current_file = true,
+		hijack_netrw_behavior = "open_default"
+	},
+	buffers = {
+		follow_current_file = true,
+		group_empty_dirs = false
+	}
+})
+vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+
--- nvim/lua/khtangent/packer.lua
+++ nvim/lua/khtangent/packer.lua
@@ -14,24 +14,33 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
-	use({
-		"Mofiqul/dracula.nvim",
-		config = function()
-			require("dracula").setup({
-				transparent_bg = true
-			})
-			vim.cmd[[colorscheme dracula]]
-		end
-	})
-	use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+	-- Utilities
 	use("tpope/vim-fugitive")
 	use("lambdalisue/suda.vim")
+	use("mg979/vim-visual-multi")
+	use("tpope/vim-surround")
+	use("preservim/nerdcommenter")
+
+	-- Navigation
 	use {
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.1',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 	use("theprimeagen/harpoon")
+	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+	use {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		}
+	}
+
+	-- Autocomplete-related
+	use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v1.x',
@@ -54,6 +63,18 @@ return require('packer').startup(function(use)
 			{'rafamadriz/friendly-snippets'}, -- Optional
 		}
 	}
+	use("mattn/emmet-vim")
+
+	-- Visual
+	use({
+		"Mofiqul/dracula.nvim",
+		config = function()
+			require("dracula").setup({
+				transparent_bg = true
+			})
+			vim.cmd[[colorscheme dracula]]
+		end
+	})
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
