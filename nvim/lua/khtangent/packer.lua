9e39645cbf898590f33e6e17abb664f8b14ea7fc
KHTangent <contact@khtangent.com>
2023-08-18 08:45:46 +0200

    Add indent-blankline

--- nvim/lua/khtangent/packer.lua
+++ nvim/lua/khtangent/packer.lua
@@ -213,6 +213,22 @@ return require('packer').startup(function(use)
 			})
 		end,
 	}
+	use {
+		"lukas-reineke/indent-blankline.nvim",
+		config = function ()
+			require("indent_blankline").setup{
+				char_highlight_list = {
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterRed",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterCyan",
+					"RainbowDelimiterViolet",
+				}
+			}
+		end
+	}
 
 	-- Fun
 	use {
