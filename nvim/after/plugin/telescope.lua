0675d60d3fa685a0f560b3d81ada894834015e40
KHTangent <contact@khtangent.com>
2023-08-29 08:38:58 +0200

    Update Telescope config

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,11 +1,13 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {desc = "Find in folder"})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {desc = "Find in Git files"})
+vim.keymap.set("n", "<C-p>", function ()
+	builtin.git_files({
+		recurse_submodules = true,
+	})
+end, {desc = "Find in Git files"})
 
-vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("Find: ") })
-end, {desc = "Grep in project"})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {desc = "Grep in project"})
 
 vim.keymap.set("n", "<leader>fc", builtin.commands, {desc = "Find command"})
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, {desc = "Find keymaps"})
