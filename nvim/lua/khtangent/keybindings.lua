d4f84bfdad4887ae7b424611d9431b00c7518ea9
KHTangent <contact@khtangent.com>
2023-10-27 07:57:20 +0200

    Improve nvim navigation on wrapped lines

--- nvim/lua/khtangent/keybindings.lua
+++ nvim/lua/khtangent/keybindings.lua
@@ -9,6 +9,9 @@ vim.keymap.set("n", "<M-k>", "<C-Y>", {desc = "Scroll up"})
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader><leader>", "zz")
