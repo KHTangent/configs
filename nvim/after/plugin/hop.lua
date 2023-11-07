0623b0b0276ca8e18c85ab9866069c29d429ba34
KHTangent <contact@khtangent.com>
2023-11-07 10:06:27 +0100

    Update hop configuration

--- nvim/after/plugin/hop.lua
+++ nvim/after/plugin/hop.lua
@@ -1,18 +1,48 @@
 local hop = require("hop")
 local directions = require("hop.hint").HintDirection
 
-vim.keymap.set("", "f", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, {remap=true})
-vim.keymap.set("", "F", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, {remap=true})
-vim.keymap.set("", "t", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, {remap=true})
-vim.keymap.set("", "T", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, {remap=true})
+vim.keymap.set("", "f", 
+	function()
+		hop.hint_char1({
+			direction = directions.AFTER_CURSOR,
+			current_line_only = true
+		})
+	end,
+	{ remap = true }
+)
 
-vim.keymap.set("n", "<leader>l", hop.hint_words, {desc = "Hop: Hint words"})
+vim.keymap.set("", "F",
+	function()
+		hop.hint_char1({
+			direction = directions.BEFORE_CURSOR,
+			current_line_only = true
+		})
+	end,
+	{ remap = true }
+)
+
+vim.keymap.set("", "t",
+	function()
+		hop.hint_char1({
+			direction = directions.AFTER_CURSOR,
+			current_line_only = true,
+			hint_offset = -1
+		})
+	end,
+	{ remap = true }
+)
+
+vim.keymap.set("", "T",
+	function()
+		hop.hint_char1({
+			direction = directions.BEFORE_CURSOR,
+			current_line_only = true,
+			hint_offset = 1
+		})
+	end,
+	{ remap = true }
+)
+
+vim.keymap.set("n", "<leader>l", hop.hint_words, {desc = "Hop to word"})
+vim.keymap.set("n", "<leader>a", hop.hint_anywhere, {desc = "Hop anywhere"})
 
