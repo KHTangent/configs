aa922f2b3ba46f3edf25644f9062ac2124927c60
KHTangent <contact@khtangent.com>
2024-05-22 15:47:59 +0200

    Fix tsserver Vue plugin

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -50,6 +50,25 @@ lsp.configure("pylsp", {
 	}
 })
 
+lsp.configure("tsserver", {
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = require("mason-registry")
+					.get_package("vue-language-server")
+					:get_install_path() .. "/node_modules/@vue/language-server",
+				languages = {"javascript", "typescript", "vue"},
+			},
+		},
+	},
+	filetypes = {
+		"javascript",
+		"typescript",
+		"vue",
+	},
+})
+
 
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
