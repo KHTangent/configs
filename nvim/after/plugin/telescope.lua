166ff8d88a727dfd1b01630cb6d3935ac4ffa492
KHTangent <contact@khtangent.com>
2024-11-13 08:48:58 +0100

    Allow opening multiple files with Telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -24,8 +24,29 @@ vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, {desc = "Find w
 vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, {desc = "Find document symbols"})
 vim.keymap.set("n", "<leader>fi", ":Telescope import<cr>", {desc = "Find imports"})
 
+-- From https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+local select_one_or_multi = function(prompt_bufnr)
+	local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+	local multi = picker:get_multi_selection()
+	if not vim.tbl_isempty(multi) then
+		require('telescope.actions').close(prompt_bufnr)
+		for _, j in pairs(multi) do
+			if j.path ~= nil then
+				vim.cmd(string.format('%s %s', 'edit', j.path))
+			end
+		end
+	else
+		require('telescope.actions').select_default(prompt_bufnr)
+	end
+end
+
 telescope.setup({
 	defaults = {
+		mappings = {
+			i = {
+				["<CR>"] = select_one_or_multi,
+			},
+		},
 		layout_config = {
 			horizontal = {
 				preview_width = 0.5
