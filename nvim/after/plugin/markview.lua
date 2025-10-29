3395d81713457829835788d1097c9ea82b57344c
KHTangent <contact@khtangent.com>
2025-10-29 07:35:27 +0100

    Switch back to render-markdown

--- nvim/after/plugin/markview.lua
+++
@@ -1,8 +0,0 @@
-local markview = require("markview")
-
-local config = require("markview.spec").default
-
-config.preview.enable_hybrid_mode = false;
-
-markview.setup(config)
-
--- nvim/lua/khtangent/lazyplugins.lua
+++ nvim/lua/khtangent/lazyplugins.lua
@@ -192,8 +192,9 @@ local plugins = {
 		opts = {},
 	},
 	{
-		"OXY2DEV/markview.nvim",
-		lazy = false,
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		opts = {},
 	},
 
 	-- Fun
