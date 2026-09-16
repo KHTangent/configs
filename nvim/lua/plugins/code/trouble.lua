4a5006f10cb4b1ae3ecfe979dea15651a7842edf
KHTangent <contact@khtangent.com>
2026-09-16 09:20:57 +0200

    Fix Trouble configuration

--- nvim/lua/plugins/code/trouble.lua
+++ nvim/lua/plugins/code/trouble.lua
@@ -4,12 +4,38 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	config = function()
-		vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end)
-		vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end)
-		vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end)
-		vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
-		vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
-		vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
-	end
+	cmd = "Trouble",
+	opts = {},
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
